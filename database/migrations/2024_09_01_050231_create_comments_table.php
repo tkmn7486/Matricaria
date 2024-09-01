056d70770c0c945068fb77b028a7e1bdc82096eb
[tkmn7486] <[tkmn7486.pro@gmail.com]>
2024-09-01 20:21:44 +0900

    add: eventsテーブル・commentsテーブル作成のため、migrationファイルを複数作成

---
+++ database/migrations/2024_09_01_050231_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id()->index();
+            $table->string('event_id');
+            $table->string('user_name')->default('名無し');
+            $table->longText('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
