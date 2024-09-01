c84202a30b46ca3defd8307f63ea30eee484b170
[tkmn7486] <[tkmn7486.pro@gmail.com]>
2024-09-01 23:16:42 +0900

    add: ルーティング設定（EventController）

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/event', 'App\Http\Controllers\EventController@getAllEvents');
+Route::get('/event/{id}', 'App\Http\Controllers\EventController@getEventById');
+Route::post('/event/create', 'App\Http\Controllers\EventController@createEvent');
