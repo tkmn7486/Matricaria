956cc05e40e9e1f0bfc7b16488dde349dc31ad29
[tkmn7486] <[tkmn7486.pro@gmail.com]>
2024-09-26 00:35:01 +0900

    add: attendテーブルを作成

---
+++ database/migrations/2024_09_25_153057_create_attend_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attend', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('ID');
+            $table->integer('event_id')->comment('イベントID');
+            $table->string('attend')->comment('参加者名');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attend');
+    }
+};
